79959407eda018890d954b5ad5df9e26cd336549
David <thedavidomole@gmail.com>
2020-10-03 14:05:33 +0100

    :pencil::green_heart: Add hero text, import google fonts to project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import "./scss/main.scss";
+import Hero from "./components/hero-section/hero-section.component";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Hero />;
 }
 
 export default App;
---
+++ src/components/hero-section/hero-section.component.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./hero-section.styles.scss";
+
+const Hero = () => (
+  <div className="hero">
+    <div className="hero-container center-text">
+      <h1>
+        We are <span>Celestial,</span>{" "}
+      </h1>
+      <p className="hero_sub">
+        we design and manufacture furniture fit for gods
+      </p>
+    </div>
+  </div>
+);
+
+export default Hero;
