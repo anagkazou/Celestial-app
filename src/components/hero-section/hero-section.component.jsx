7748d856beaacb8ac213b0aa69ee33f44f6eb81c
David <thedavidomole@gmail.com>
2020-10-03 20:51:00 +0100

    :green_heart::heavy_plus_sign: Finish main hero content, install react-router-dom

--- src/components/hero-section/hero-section.component.jsx
+++ src/components/hero-section/hero-section.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./hero-section.styles.scss";
+import { Link } from "react-router-dom";
 
 const Hero = () => (
   <div className="hero">
@@ -7,9 +8,12 @@ const Hero = () => (
       <h1>
         We are <span>Celestial,</span>{" "}
       </h1>
-      <p className="hero_sub">
+      <p className="hero__sub">
         we design and manufacture furniture fit for gods
       </p>
+      <Link to="/shop" className="hero__cta">
+        Discover Our Collections
+      </Link>
     </div>
   </div>
 );
