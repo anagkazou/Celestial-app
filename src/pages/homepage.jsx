b3c76bfb414fdc97f9a3954ca02bbb68ee474a7f
David <thedavidomole@gmail.com>
2020-10-06 13:31:41 +0100

    Redesign about-text

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -9,16 +9,21 @@ const HomePage = () => (
     <section className="section__about">
       <div className="about__content">
         <div className="about__content--text">
-          <h1 className="title">
-            Who <br /> We Are
-          </h1>
-          <p className="paragraph">
+          <p className="title">Who We Are</p>
+          <h2 className="title__sub">
             {" "}
+            <span>True</span> furniture Masters
+          </h2>
+          <p className="paragraph">
             We are a collaboration of emerging and iconic designers and makers.
             Each object was designed to bring quality and style to your everyday
             rituals.
           </p>
         </div>
+
+        {/* <section className="section__browse-collections">
+
+    </section> */}
         <div className="about__content--img img">
           <img src={imgAbout1} alt="" className="img-1" />
           <img src={imgAbout2} alt="" className="img-2" />
