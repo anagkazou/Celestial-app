aa507f1c1286f059b4eb13bf3da06613b343d090
David <thedavidomole@gmail.com>
2020-10-07 16:16:42 +0100

    :heavy_plus_sign:Install jquery

--- src/assets/img/images.js
+++ src/assets/img/images.js
@@ -6,3 +6,26 @@ export const fifth = require("./carousel-5.png");
 
 export const imgAbout1 = require("./about-1.jpg");
 export const imgAbout2 = require("./about-2.jpg");
+
+export const imgChair = {
+  first: require("./chair-1.jpg"),
+  second: require("./chair-2.jpg"),
+  third: require("./chair-3.jpg"),
+};
+export const imgLight = {
+  first: require("./light-1.jpg"),
+  second: require("./light-2.jpg"),
+  third: require("./light-3.jpg"),
+};
+// export const imgSofa = {
+//   first: require("./sofa-1.jpg"),
+//   second: require("./sofa-2.jpg"),
+//   third: require("./sofa-3.jpg"),
+//   fourth: require("./sofa-4.jpg"),
+// };
+// export const imgTable = {
+//   first: require("./table-1.jpg"),
+//   second: require("./table-2.jpg"),
+//   third: require("./table-3.jpg"),
+//   fourth: require("./table-4.jpg"),
+// };
--- src/components/collections-preview/collections-preview.component.jsx
+++ src/components/collections-preview/collections-preview.component.jsx
@@ -1,14 +1,69 @@
 import React from "react";
 import "./collection-preview.styles.scss";
+import { Link } from "react-router-dom";
+
+import { imgChair, imgLight } from "../../assets/img/images";
 
 const CollectionPreview = () => (
   <section className="section__browse-collections">
     <p className="title">Browse Collections</p>
     <div className="preview">
-      <div className="preview__right"></div>
-      <div className="preview__text"></div>
+      <div className="preview__left img">
+        <img
+          src={imgChair.second}
+          className="img__small"
+          style={{ left: "50%", top: "12%" }}
+        />
+        <img
+          src={imgChair.third}
+          className="img__small"
+          style={{ bottom: "20%", left: "20%" }}
+        />
+        <img
+          src={imgLight.third}
+          className="img__large"
+          style={{ top: "5%", right: "50%", transform: "translateX(50%)" }}
+        />
+      </div>
 
-      <div className="preview__left"></div>
+      <div className="preview__text">
+        <Link className="collection-title" id="1">
+          Chairs
+        </Link>
+        <Link className="collection-title" id="2">
+          Lights
+        </Link>
+        <Link className="collection-title" id="3">
+          Sofas
+        </Link>
+        <Link className="collection-title" id="4">
+          Tables
+        </Link>
+        <Link className="collection-title" id="5">
+          Stools
+        </Link>
+
+        <Link to="/shop" className="btn btn__text">
+          View everything
+        </Link>
+      </div>
+      <div className="preview__right img">
+        <img
+          src={imgChair.first}
+          className="img__large"
+          style={{ top: "5%", right: "50%", transform: "translateX(50%)" }}
+        />
+        <img
+          src={imgLight.first}
+          className="img__small"
+          style={{ bottom: "20%", right: "20%" }}
+        />
+        <img
+          src={imgLight.second}
+          className="img__small"
+          style={{ right: "50%", top: "12%" }}
+        />
+      </div>
     </div>
   </section>
 );
