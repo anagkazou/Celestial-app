64787915baf1035028688955b9a4b1b654eec998
David <thedavidomole@gmail.com>
2020-11-18 12:54:12 +0100

    Implement Quick add button on shop items

--- src/components/shop-item/shop-item.component.jsx
+++ src/components/shop-item/shop-item.component.jsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import './shop-item.styles.scss';
 
+import CustomButton from '../custom-button/custom-button.component'
 const ShopItem = (props) =>{
    
 
         return(
             <React.Fragment>
                 <div className="furniture-item">
-<div className="furniture-item__img" style= {{
-    backgroundImage:`url(${props.imageUrl})`
-}}/>
+                    <div className="furniture-item__img" style= {{
+                        backgroundImage:`url(${props.imageUrl})`
+                    }}/>
+                    <div className="button-container">
+                    <CustomButton>Quick Add</CustomButton>
+                    </div>
+                    <p className="furniture-item__category">
+                        {props.category}
+                    </p>
 
-<p className="furniture-item__category">
-    {props.category}
-</p>
-
-<div className="furniture-item__details">
-    <p className="furniture-item__name">{props.name}</p>
-<p className="furniture-item__price">{`₦${props.price}`}</p>
-</div>
+                    <div className="furniture-item__details">
+                        <p className="furniture-item__name">{props.name}</p>
+                    <p className="furniture-item__price">{`₦${props.price}`}</p>
+                    </div>
 
     </div>
             </React.Fragment>
