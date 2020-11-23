d5118df822e8fe429f9311018943d68e1e822e64
David <thedavidomole@gmail.com>
2020-11-23 17:41:10 +0100

    Style cart preview component

--- src/components/cart-preview/cart-preview.component.jsx
+++ src/components/cart-preview/cart-preview.component.jsx
@@ -10,17 +10,25 @@ import CartItem from '../cart-item/cart-item.component'
 
 const CartPreview = ({toggleCartHidden, cartItems}) => (
     <div className="cart-preview">
-        <div className="btn--close" onClick={toggleCartHidden}>
+<div className="cart-preview__block--top">
+                <div className="cart-title">Shopping Cart</div>
+<div className="btn--close" onClick={toggleCartHidden}>
             <img src={CloseButton} alt="close"/>
         </div>
+</div>
+
+        
         <div className="cart-items">
         {
             cartItems.map(cartItem => <CartItem key={cartItem.id} item = {cartItem}/>)
         }
         </div>
-          
-        <div className="cart-info"></div>
+
+        <div className="cart-preview__block--bottom">
+            <p className="promo-text">Sign up for 10% off your first order. Valid within two days of signup.</p>
         <CustomButton>CONTINUE TO CHECKOUT</CustomButton>
+        </div>
+          
     </div>
 )
 
