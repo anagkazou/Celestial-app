3df791468aa5f79d1d8f380ac71db761704c36f4
David <thedavidomole@gmail.com>
2020-11-23 08:13:12 +0100

    Implement displaying cart items in cart preview

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import "./cart-item.styles.scss"
 
-const CartItem = ({item: {imageUrl,price, name, quantity}}) => (
+const CartItem = ({item: {imageUrl,price, name, quantity, category}}) => (
     <div className="cart-item">
         <img src={imageUrl} alt="item"/>
         <div className="item-details">
             <div className="item-details__block">
-                <span className="item-name">{name}</span>
-            <span className="item-price">
-                {quantity} Piece(s)
-            </span>
+                <div className="item-name">{name}</div>
+            <div className="item-price">
+$ {price}            </div>
             </div>
+<div className="item-category">{category}</div>
+<div className="item-details__block">
+            <div className="item-quantity">
+                {quantity} Piece(s)
+            </div>        
+                    <div className="btn-delete">DELETE</div>
 
-
+            </div>
         </div>
     </div>
 );
--- src/components/cart-preview/cart-preview.component.jsx
+++ src/components/cart-preview/cart-preview.component.jsx
@@ -6,21 +6,29 @@ import {connect} from 'react-redux';
 
 import {toggleCartHidden} from '../../redux/cart/cart.actions';
 
-const CartPreview = ({toggleCartHidden}) => (
+import CartItem from '../cart-item/cart-item.component'
+
+const CartPreview = ({toggleCartHidden, cartItems}) => (
     <div className="cart-preview">
         <div className="btn--close" onClick={toggleCartHidden}>
             <img src={CloseButton} alt="close"/>
         </div>
         <div className="cart-items">
-
+        {
+            cartItems.map(cartItem => <CartItem key={cartItem.id} item = {cartItem}/>)
+        }
         </div>
+          
         <div className="cart-info"></div>
         <CustomButton>CONTINUE TO CHECKOUT</CustomButton>
     </div>
 )
 
+const mapStateToProps = ({cart: {cartItems}}) => ({
+cartItems})
+
 const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 })
 
-export default  connect(null, mapDispatchToProps)(CartPreview);
+export default  connect(mapStateToProps, mapDispatchToProps)(CartPreview);
