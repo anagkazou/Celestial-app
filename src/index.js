16d57a9a7171208a7b9f3dc0a37dc4c3da41beb9
David <thedavidomole@gmail.com>
2020-12-09 14:36:57 +0100

    implement saving cart state in localtorage with redux persist

--- src/index.js
+++ src/index.js
@@ -5,13 +5,17 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
-import store from './redux/store';
+import {store, persistor} from './redux/store';
+
+import {PersistGate} from 'redux-persist/integration/react';
 
 ReactDOM.render(
   <>
  <Provider store = {store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor ={persistor}>    
+        <App />
+    </PersistGate>
     </BrowserRouter>
     </Provider>
     </>,
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,21 @@
 import {combineReducers} from 'redux';
 
-import userReducer from "./user/user-reducer"
-import cartReducer from './cart/cart.reducer'
-export default combineReducers({
+import {persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+import userReducer from "./user/user-reducer";
+import cartReducer from './cart/cart.reducer';
+
+const persistConfig = { 
+    key:'root',
+    storage, 
+    whitelist: ["cart"]
+}
+
+const rootReducer = combineReducers({
 
     user:userReducer,
     cart:cartReducer
-})
+});
+
+export default persistReducer(persistConfig, rootReducer);
