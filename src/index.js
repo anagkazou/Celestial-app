11a0e5579cbc54f5d39b98e4b216c57627061abb
David <thedavidomole@gmail.com>
2021-01-07 15:28:42 +0100

    uninstall smooth-scrollbar

--- src/index.js
+++ src/index.js
@@ -10,8 +10,9 @@ import {store, persistor} from './redux/store';
 import {PersistGate} from 'redux-persist/integration/react';
 
 import Scrollbar from "smooth-scrollbar";
+Scrollbar.init(document.querySelector('#scrollbar'));
 
-Scrollbar.init(document.querySelector("#customScroll"));
+
 
 ReactDOM.render(
   <>
@@ -26,8 +27,8 @@ ReactDOM.render(
     </>,
 
   document.getElementById("root")
+  
 );
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
