51217e11197b1decd104f325945986ae5210eca4
davidomole <thedavidomole#gmail.com>
2021-06-03 14:28:48 +0100

    fixed scroll behaviour bug

--- src/pages/shop/sofas.component.jsx
+++ src/pages/shop/sofas.component.jsx
@@ -1,16 +1,13 @@
 import React, { useEffect } from "react";
-import { useLocation } from "react-router-dom";
-import ShopTile from "../../components/shop-title/shop-title.component";
+ import ShopTile from "../../components/shop-title/shop-title.component";
 import ItemsShowcase from "../../components/showcase/showcase.component";
 import "./shop.styles.scss";
 
 const Sofas = () => {
-  const { pathname } = useLocation();
-
-  useEffect(() => {
  
-  }, [pathname]);
-
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [ ]);
   return (
     <>
       <ShopTile category="Sofas" />
