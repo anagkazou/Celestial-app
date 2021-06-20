9c3ab29e5c4b1b4af3cd6034e339b58f7776260e
DavidOmole <thedavidomole@gmail.com>
2021-06-20 17:12:02 +0100

    implement code to send shop data to firebase

---
+++ src/redux/shop/shop.reducer.js
@@ -0,0 +1,20 @@
+import SHOP_DATA from "../../pages/shop/shop.data";
+import ShopActionTypes from "./shop.types";
+
+const INITIAL_STATE = {
+  collections: SHOP_DATA,
+};
+
+const shopReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case ShopActionTypes.UPDATE_COLLECTIONS:
+      return {
+        ...state,
+        collections: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default shopReducer;
